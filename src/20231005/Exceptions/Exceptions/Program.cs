1338db4f2469138a4f1b5aa89913aa0f5a1ef45a
Atilla  (Home) <atilla@kati.bz>
2023-10-05 21:33:22 +0200

    try-catch Einführung

---
+++ src/20231005/Exceptions/Exceptions/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exceptions
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int alter = 0;
+
+
+            Console.Write("Alter eingeben: ");
+
+            try
+            {
+                alter = int.Parse(Console.ReadLine());
+            }
+            catch(OverflowException oex) 
+            {
+                Console.WriteLine(oex.Message);
+            }
+            catch (FormatException ex)
+            {
+
+            }
+            catch(Exception error) 
+            {
+                Console.WriteLine("\n\aERROR: " + error.Message);
+            }
+
+
+            Console.WriteLine("Ende.");
+        }
+    }
+}
