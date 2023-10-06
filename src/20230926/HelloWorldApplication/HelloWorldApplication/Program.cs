ffc0c6bcc5625d44906227bd3307c04b4d7c9127
Marco Wagner <marco.wagner@prismalife.com>
2023-10-06 14:36:11 +0200

    Upgraded to .NET 7

--- src/20230926/HelloWorldApplication/HelloWorldApplication/Program.cs
+++ src/20230926/HelloWorldApplication/HelloWorldApplication/Program.cs
@@ -1,19 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿Console.WriteLine("Hallo liebe Leute....");
+Console.WriteLine("C# ist toll...");
 
-namespace HelloWorldApplication
-{
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hallo liebe Leute....");
-            Console.WriteLine("C# ist toll...");            
-
-            Console.ReadLine();
-        }
-    }
-}
+Console.ReadLine();
