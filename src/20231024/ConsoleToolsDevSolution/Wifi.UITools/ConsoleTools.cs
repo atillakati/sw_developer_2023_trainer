9c2132354fc74e9dc95a3ca05730c046f1e087fa
Atilla  (Home) <atilla@kati.bz>
2023-10-24 20:16:36 +0200

    Exaples with Class-Libraries; ConsoleTools

---
+++ src/20231024/ConsoleToolsDevSolution/Wifi.UITools/ConsoleTools.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wifi.UITools
+{
+    /// <summary>
+    /// Sammelsurium von Methoden für die Arbeit in der Console.
+    /// </summary>
+    public abstract class ConsoleTools 
+    {
+        /// <summary>
+        /// Liefert eine Eingabe als string zurück. string.Empty und NULL werden nicht akzeptiert.
+        /// </summary>
+        /// <param name="inputPrompt">Text für die Eingabeaufforderung</param>
+        /// <returns></returns>
+        public static string ReadString(string inputPrompt)
+        {
+            string input = string.Empty;
+
+            do
+            {
+                Console.Write(inputPrompt);
+                input = Console.ReadLine();
+            }
+            while (string.IsNullOrEmpty(input));
+
+            return input;
+        }
+
+        public static int ReadInt(string inputPrompt)
+        {
+            string input = string.Empty;
+            int inputValue = 0;
+            bool inputIsValid = false;
+
+            do
+            {
+                Console.Write(inputPrompt);
+                input = Console.ReadLine();
+
+                try
+                {
+                    inputValue = int.Parse(input);
+                    inputIsValid = true;
+                }
+                catch
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("\aERROR: Ungültige Eingabe.");
+                    inputValue = 0;
+                    Console.ResetColor();
+                    inputIsValid = false;
+                }
+            }
+            while (!inputIsValid);
+
+            return inputValue;
+        }
+
+        public static DateTime ReadDateTime(string inputPrompt)
+        {
+            string input = string.Empty;
+            DateTime inputDateTime = DateTime.MinValue;
+            bool inputIsValid = false;
+
+            do
+            {
+                Console.Write(inputPrompt);
+                input = Console.ReadLine();
+
+                try
+                {
+                    inputDateTime = DateTime.ParseExact(input, "dd.MM.yyyy", CultureInfo.InvariantCulture);
+                    inputIsValid = true;
+                }
+                catch
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("\aERROR: Ungültige Datumseingabe.");
+                    inputDateTime = DateTime.MinValue;
+                    Console.ResetColor();
+                    inputIsValid = false;
+                }
+            }
+            while (!inputIsValid);
+
+            return inputDateTime;
+        }
+
+        /// <summary>
+        /// Generiert eine Überschrift mit einem Titel.       
+        /// </summary>
+        /// <param name="headerText">Der Titel der in der Überschrift dargestellt werden soll.</param>
+        /// <param name="headerTextColor">Die Farbe in welcher der Titel dargestellt werden soll.</param>
+        /// <param name="clearScreen">true, wenn der Bildschirm vor der Ausgabe gelöscht werden soll, sonst false.</param>
+        public static void CreateHeader(string headerText, ConsoleColor headerTextColor, bool clearScreen)
+        {
+            int xTitelPos = 0;
+
+            //soll der bildschirm gelöscht werden?
+            if (clearScreen)
+            {
+                Console.Clear();
+            }
+
+            //Darstellung Programm Header            
+            Console.WriteLine(new string('*', Console.WindowWidth - 1));
+            Console.WriteLine(new string('*', Console.WindowWidth - 1));
+            Console.WriteLine(new string('*', Console.WindowWidth - 1));
+
+            //cursor f. Titel Ausgabe positionieren
+            xTitelPos = (Console.WindowWidth - (headerText.Length + 2)) / 2;
+            Console.SetCursorPosition(xTitelPos, 1);
+
+            ConsoleColor oldColor = Console.ForegroundColor;
+            Console.ForegroundColor = headerTextColor;
+            Console.Write(" " + headerText + " ");
+            Console.ForegroundColor = oldColor;
+
+            //alte cursor Position wiederherstellen
+            Console.SetCursorPosition(0, 4);
+            //Consolen Fensterbezeichnung setzen
+            Console.Title = headerText;
+        }
+
+    }
+}
---
+++ src/20231024/ConsoleToolsDevSolution/Wifi.UITools/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Wifi.UITools")]
+[assembly: AssemblyDescription("Wifi Console spezific tools")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Wifi.UITools")]
+[assembly: AssemblyCopyright("Copyright ©WIFI 2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("8080aea6-9b7c-4de3-9764-3c3245855833")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("2023.0.10.24")]
+[assembly: AssemblyFileVersion("2023.0.10.24")]
