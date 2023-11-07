6aa41cbfb57133f96a8fd15f7d0f473e6187941c
Atilla  (Home) <atilla@kati.bz>
2023-11-07 21:25:24 +0100

    Beispiel Verebung (klassisch)

---
+++ src/20231107/VererbungBsp/VererbungBsp/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using VererbungBsp.Shapes;
+
+namespace VererbungBsp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {   
+            var myShapeList = new Shape[]
+            {
+                new Shape("Fredy", 5, ConsoleColor.Red),
+                new Vieleck("Ecki", 10, ConsoleColor.Yellow),
+                new Kreis("Olga", ConsoleColor.Blue),
+                new Dreieck("Triss", ConsoleColor.Green),
+            };
+           
+            DisplayShapes(myShapeList);            
+        }
+
+        private static void DisplayShapes(Shape[] myShapeList)
+        {
+            foreach (Shape myShape in myShapeList)
+            {
+                myShape.Draw();
+            }
+        }
+    }
+}
---
+++ src/20231107/VererbungBsp/VererbungBsp/Shapes/Kreis.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace VererbungBsp.Shapes
+{
+    internal class Kreis : Shape
+    {
+        public Kreis(string description, ConsoleColor color)
+            : base("Kreis " + description, 0, color)
+        {
+            
+        }
+    }
+}
