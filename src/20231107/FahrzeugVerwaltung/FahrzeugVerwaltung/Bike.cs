c0b80b044c3695c94930f0ebc243feb5cb31524c
Atilla  (Home) <atilla@kati.bz>
2023-11-07 20:29:50 +0100

    klassische Vererbung - IST-EIN Beziehung

---
+++ src/20231107/FahrzeugVerwaltung/FahrzeugVerwaltung/Bike.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FahrzeugVerwaltung
+{
+    internal class Bike : Vehicle
+    {
+		private int _suspensionTravel;
+
+        public Bike() 
+            : base("Vespa Std.", 40, 5, ConsoleColor.Red)
+        {
+            _suspensionTravel = 180;
+        }
+        
+        public Bike(string bikeDescription, int ps, int maxSpeed, ConsoleColor bikeColor, int suspensionTravel)
+            : base(bikeDescription, maxSpeed, ps, bikeColor)
+        {
+            _suspensionTravel = suspensionTravel;            
+        }
+
+        public int SuspensionTravel
+		{
+			get { return _suspensionTravel; }			
+		}
+
+	}
+}
---
+++ src/20231107/FahrzeugVerwaltung/FahrzeugVerwaltung/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace FahrzeugVerwaltung
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Vehicle[] myVehicleList = new Vehicle[]
+            {
+                new Vehicle(),
+                new Vehicle("Super Badmobil", 250, 512, ConsoleColor.Cyan),
+                new Vehicle("Schoolbus #1", 95, 308, ConsoleColor.Yellow),
+                new Bike(),
+                new Bike("Moto Cross", 80, 135, ConsoleColor.Red, 250)
+            };
+
+            foreach (var vehicle in myVehicleList)
+            {
+                //Fahrzeug Farbe setzen
+                Console.ForegroundColor = vehicle.Color;
+
+                //Fahrzeug beschleunigen
+                vehicle.SpeedUp(vehicle.MaxSpeed / 2);
+
+                //Ausgabe
+                vehicle.ShowInfo();
+                Console.WriteLine();
+            }
+
+            Console.ResetColor();
+
+            //Bike bike = new Bike();
+            //Bike cross = new Bike("Moto Cross", 80, 135, ConsoleColor.Red, 250);
+
+            //bike.ShowInfo();
+            //cross.ShowInfo();
+        }
+    }
+}
