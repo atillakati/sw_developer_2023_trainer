5eecd467a92bb221781f4d3b583f9f47dd12c8b7
Atilla  (Home) <atilla@kati.bz>
2023-11-09 19:29:30 +0100

    Vererbung HAT-EIN

---
+++ src/20231109/Vererbung_HAT_EIN/Vererbung_HAT_EIN/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Vererbung_HAT_EIN.VehicleTypes;
+
+namespace Vererbung_HAT_EIN
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Radio radio = new Radio();
+
+            radio.ShowState();
+            radio.Play();
+
+            radio.ChangePowerState(DevicePower.On);
+            radio.Frequency = 102.0;
+
+            radio.ShowState();
+            radio.Play();
+
+            var meinAuto = new Vehicle();
+
+            meinAuto.SetEntertainmentPower(true);
+            Console.WriteLine(meinAuto.VehicleRadio.Frequency);
+        }
+    }
+}
---
+++ src/20231109/Vererbung_HAT_EIN/Vererbung_HAT_EIN/VehicleTypes/Vehicle.cs
@@ -0,0 +1,98 @@
+﻿using System;
+
+namespace Vererbung_HAT_EIN.VehicleTypes
+{
+    internal class Vehicle
+    {
+        private string _description;
+        private ConsoleColor _color;
+        private int _ps;
+        private int _currentSpeed;
+        private int _maxSpeed;
+
+        private Radio _vehicleRadio;
+
+        public Vehicle()
+        {
+            _description = "Family Car";
+            _color = ConsoleColor.Blue;
+            _ps = 140;
+            _maxSpeed = 187;
+            _currentSpeed = 0;
+            _vehicleRadio = new Radio();
+        }
+
+        public Vehicle(string description, int maxSpeed, int ps, ConsoleColor color)
+        {
+            _description = description;
+            _maxSpeed = maxSpeed;
+            _ps = ps;
+            _color = color;
+            _currentSpeed = 0;
+            _vehicleRadio = new Radio();
+        }
+
+
+        
+        public int MaxSpeed
+        {
+            get { return _maxSpeed; }
+        }
+
+        public int CurrentSpeed
+        {
+            get { return _currentSpeed; }
+        }
+
+        public int Ps
+        {
+            get { return _ps; }
+        }
+
+        public ConsoleColor Color
+        {
+            get { return _color; }
+            set { _color = value; }
+        }
+
+        public string Description
+        {
+            get { return _description; }
+        }
+
+
+        public void SetEntertainmentPower(bool isOn)
+        {
+            if (isOn)
+            {
+                _vehicleRadio.ChangePowerState(DevicePower.On);
+            }   
+            else
+            {
+                _vehicleRadio.ChangePowerState(DevicePower.Off);
+            }
+        }
+
+        public void SpeedUp(int delta)
+        {
+            if (_currentSpeed + delta < 0)
+            {
+                _currentSpeed = 0;
+            }
+            else if (_currentSpeed + delta > _maxSpeed)
+            {
+                _currentSpeed = _maxSpeed;
+            }
+            else
+            {
+                _currentSpeed += delta;
+            }
+        }
+
+        public void ShowInfo()
+        {
+            Console.WriteLine($"{_description.ToUpper()} - {_ps} PS");
+            Console.WriteLine($"Speed: {_currentSpeed} of max. {_maxSpeed} km/h");
+        }
+    }
+}
