7cbf19729ef31cf51c697f1ad0491442d925b46a
Atilla  (Home) <atilla@kati.bz>
2023-11-28 19:45:51 +0100

    Create unit tests for Mp3Item class

--- src/Wifi.Playlist.Solution/Wifi.Playlist.Items.Test/Mp3ItemTests.cs
+++ src/Wifi.Playlist.Solution/Wifi.Playlist.Items.Test/Mp3ItemTests.cs
@@ -11,9 +11,12 @@ namespace Wifi.Playlist.Items.Test
     public class Mp3ItemTests
     {
         [Test]
+        [Ignore("Testmethod for debugging TagLib nuget")]
         public void TestMp3Item_class()
         {
-            var mp3Item = new Mp3Item();
+            var mp3Item = new Mp3Item(@"C:\myCode\Repos\sw_developer_2023_trainer\DemoFiles\001 - Bruno Mars - Grenade.mp3");
+
+            var title = mp3Item.Title;
         }
     }
 }
