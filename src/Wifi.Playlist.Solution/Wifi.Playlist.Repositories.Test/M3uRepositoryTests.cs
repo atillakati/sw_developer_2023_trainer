8e536f3b5d807873315f489f9e91376221915e42
Atilla Kati @ Home <atilla@kati.bz>
2023-12-04 19:06:46 +0100

    Add base m3u implementation

---
+++ src/Wifi.Playlist.Solution/Wifi.Playlist.Repositories.Test/M3uRepositoryTests.cs
@@ -0,0 +1,55 @@
+﻿using Moq;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.IO.Abstractions;
+using System.IO.Abstractions.TestingHelpers;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Wifi.Playlist.CoreTypes;
+
+namespace Wifi.Playlist.Repositories.Test
+{
+    [TestFixture]
+    public class M3uRepositoryTests
+    {
+        private M3uRepository _fixture;   
+        private MockFileSystem _mockFileSystem;
+        private Mock<IPlaylist> _mockedPlaylist;        
+
+        [SetUp]
+        public void Init()
+        {
+            _mockFileSystem = new MockFileSystem();            
+
+            var playlistItem1 = new Mock<IPlaylistItem>();
+            playlistItem1.Setup(x => x.Title).Returns("Song title 1");
+            playlistItem1.Setup(x => x.Author).Returns("Super singer 1");
+            playlistItem1.Setup(x => x.Duration).Returns(TimeSpan.FromSeconds(15));
+            playlistItem1.Setup(x => x.FilePath).Returns("song1.mp3");
+
+            var playlistItem2 = new Mock<IPlaylistItem>();
+            playlistItem2.Setup(x => x.Title).Returns("Song title 1");
+            playlistItem2.Setup(x => x.Author).Returns("Super singer 1");
+            playlistItem2.Setup(x => x.Duration).Returns(TimeSpan.FromSeconds(15));
+            playlistItem2.Setup(x => x.FilePath).Returns("song1.mp3");
+
+            _mockedPlaylist = new Mock<IPlaylist>();
+            _mockedPlaylist.Setup(x => x.Name).Returns("My super charts 2022");
+            _mockedPlaylist.Setup(x => x.Author).Returns("Gandalf");
+            _mockedPlaylist.Setup(x => x.CreatedAt).Returns(DateTime.Now);
+            _mockedPlaylist.Setup(x => x.Items).Returns(new IPlaylistItem[] { playlistItem1.Object, playlistItem2.Object });
+            
+            _fixture = new M3uRepository(_mockFileSystem);
+        }
+
+        [Test]
+        public void Save()
+        {
+            _fixture.Save(_mockedPlaylist.Object, "myPlaylist.m3u");
+
+            Assert.That(_mockFileSystem.AllFiles.Count, Is.EqualTo(1)); 
+        }
+    }
+}
--- src/Wifi.Playlist.Solution/Wifi.Playlist.Repositories/M3uRepository.cs
+++ src/Wifi.Playlist.Solution/Wifi.Playlist.Repositories/M3uRepository.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using PlaylistsNET.Content;
+using PlaylistsNET.Models;
+using System;
 using System.Collections.Generic;
+using System.IO.Abstractions;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,9 +12,17 @@ namespace Wifi.Playlist.Repositories
 {
     public class M3uRepository : IPlaylistRepository
     {
-        public string Description => throw new NotImplementedException();
+        private readonly IFileSystem _fileSystem;
 
-        public string Extension => throw new NotImplementedException();
+        public M3uRepository() : this(new FileSystem()) { }
+
+        public M3uRepository(IFileSystem fileSystem)
+        {
+            _fileSystem = fileSystem;
+        }
+        public string Description => "M3U Playlist format";
+
+        public string Extension => ".m3u";
 
         public IPlaylist Load(string filePath)
         {
@@ -20,7 +31,33 @@ namespace Wifi.Playlist.Repositories
 
         public void Save(IPlaylist playlist, string filePath)
         {
-            throw new NotImplementedException();
+            var m3uPlaylist = new M3uPlaylist();
+            m3uPlaylist.IsExtended = true;
+
+            m3uPlaylist.Comments.Add($"#Title:{playlist.Name}");
+            m3uPlaylist.Comments.Add($"#Author:{playlist.Author}");
+            m3uPlaylist.Comments.Add($"#CreatedAt:{playlist.CreatedAt}");
+
+            foreach (var item in playlist.Items)
+            {
+                var entityItem = new M3uPlaylistEntry()
+                {
+                    AlbumArtist = item.Author,
+                    Duration = item.Duration,
+                    Path = item.FilePath,
+                    Title = item.Title
+                };
+
+                m3uPlaylist.PlaylistEntries.Add(entityItem);
+            }
+            
+            var content = new M3uContent();
+            string text = content.ToText(m3uPlaylist);
+
+            using(var stream = _fileSystem.File.CreateText(filePath))
+            {                
+                stream.WriteLine(text);
+            }
         }
     }
 }
