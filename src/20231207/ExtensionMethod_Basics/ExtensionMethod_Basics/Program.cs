ab01729ec05f26570fc1bc571a84d87f34c78a6d
Atilla  (Home) <atilla@kati.bz>
2023-12-07 18:34:54 +0100

    Extension Method basics

---
+++ src/20231207/ExtensionMethod_Basics/ExtensionMethod_Basics/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExtensionMethod_Basics
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double einWert = 5.96548;            
+
+            //Console.WriteLine($"Der Artikel kostet: EUR {einWert:F2}");
+
+            Console.WriteLine($"Der Artikel kostet: {einWert.ToCurrency()}");
+        }        
+    }
+}
